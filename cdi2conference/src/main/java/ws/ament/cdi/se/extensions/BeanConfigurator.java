530a01b0b6a5d0f4249b501a12f2dd863b20a96a
John D. Ament <john.d.ament@gmail.com>
2017-03-20 10:42:05 -0700

    Adding a bean discovery example.  Updating package.json to run for later presentation.

--- cdi2conference/src/main/java/ws/ament/cdi/se/extensions/BeanConfigurator.java
+++ cdi2conference/src/main/java/ws/ament/cdi/se/extensions/BeanConfigurator.java
@@ -1,17 +1,20 @@
 package ws.ament.cdi.se.extensions;
 
-import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.AfterBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 
 /**
  * Created by johnament on 3/13/17.
  */
 public class BeanConfigurator implements Extension {
-    public void onBean(@Observes AfterBeanDiscovery afterBeanDiscovery) {
-        afterBeanDiscovery.addBean()
-                .produceWith(instance -> new Object())
-                .destroyWith((o, cc) -> {})
-                ;
-    }
+
+//    public void onAnnotatedType(@Observes ProcessAnnotatedType<?> pat) {
+//        pat.configureAnnotatedType().fields().stream()
+//    }
+//
+//    public void onBean(@Observes AfterBeanDiscovery afterBeanDiscovery) {
+//        afterBeanDiscovery.addBean()
+//                .produceWith(instance -> new Object())
+//                .destroyWith((o, cc) -> {})
+//                ;
+//    }
 }
---
+++ cdi2conference/src/main/java/ws/ament/cdi/se/extensions/BeanDiscoverer.java
@@ -0,0 +1,27 @@
+package ws.ament.cdi.se.extensions;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessBean;
+import javax.inject.Singleton;
+
+public class BeanDiscoverer implements Extension {
+    public void onBeans(@Observes ProcessBean<?> pb) {
+
+    }
+
+    @Singleton
+    public static class Producers {
+
+    }
+
+    public static void main(String...args) {
+        final SeContainer initialize = SeContainerInitializer.newInstance()
+                .initialize();
+        final Producers producers = initialize.select(Producers.class).get();
+        assert producers == null;
+        initialize.close();
+    }
+}
